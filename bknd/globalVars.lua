d55e344a01401b3b806107433955d980dae08c9a
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-02-15 07:10:05 -0600

    iniital commit

---
+++ bknd/globalVars.lua
@@ -0,0 +1,5 @@
+local input = io.read()
+local write = io.write()
+local errorMessage = error("There has been an error that has occured. Please try again.")
+
+-- There may be more global varibles that appear in here later the project goes on.
